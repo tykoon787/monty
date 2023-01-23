d101316148a5a85e304ce2454437d9afc35f7c13
tykoon787 <tykoon787@protonmail.com>
2023-01-23 15:26:53 +0300

    Updated Code

--- tests/task1.c
+++ tests/task1.c
@@ -2,9 +2,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+/* SYS CALL*/
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+
+/* strstr */
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
@@ -32,9 +36,44 @@ int main(int argc, char *argv[])
 					{
 						line_buffer[line_buffer_index] = '\0';
 						printf("Line %d: %s\n", line_number, line_buffer);
+						
+						/* Tokenize Buffer to check for push */
+						/* This check is used to determine if the strin "push" is present at the beginning of the 
+						buffer. 
+						
+						This is done by Compare the return value of strstr() to the original buffer */
+						char *p = strstr(line_buffer, "push");
+						if ((p))
+						{
+							/*move pointer to the location of the value in the buffer*/
+							p = p + strlen("push"); // p += strlen("push")
+
+							/*convert the value in the buffer to an interger using strtol()*/
+							int value = strtol(p, NULL, 10);
+
+							/* Push Value to the stack )*/
+							printf("Function to push with value %d \n", value);
+						}
+
+						/* Tokenize Buffer to check for pop */
+						else if (strstr(line_buffer, "pop") == line_buffer)
+						{
+							printf("Function Pop Called \n");
+						}
+
+						/* Tokenize Buffer to check for pall */
+						else if (strstr(line_buffer, "pall") == line_buffer)
+						{
+							printf("Function Pall Called \n");
+						}
+						// Insert Error Checking Code 
+						/* 
+						{
+							error check
+						}
+						*/
 						line_number++;
 						line_buffer_index = 0;
-						// Insert code for error checking
 					}
 					else
 					{
