52a7af84ec1426e8fac1cb8ddcb61a9021138fd9
tykoon787 <tykoon787@protonmail.com>
2023-01-23 20:03:25 +0300

    Editted the push(), pall(), pop() functions

--- tests/monty.h
+++ tests/monty.h
@@ -31,4 +31,10 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+/* ==== FUNCTIONS =====*/
+void *createNode(int x);
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+
 #endif /* MONTY_H */
