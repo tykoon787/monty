a2f07a53aaaa7a00a99bd3b669819a902b1f05be
tykoon787 <tykoon787@protonmail.com>
2023-01-29 16:03:12 +0300

    Updated all functions

--- tests/op_funcs.h
+++ tests/op_funcs.h
@@ -3,41 +3,51 @@
 
 #include "monty.h"
 #include "monty_interpreter.h"
+#include "callFuncs.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-stack_t *top = NULL;
-stack_t *head = NULL;
+extern stack_t **stack;
+
+const char *VALID_INSTRUCTIONS[] = {"push", "pall", "pop"};
 
 /**
  * createNode - Funcition to Create  a New Node
  * @x: Value to be inserted in New Node
- * return: Nothing
+ * Return: A void pointer to newNode
 */
 void *createNode(int x)
 {
-	printf("Creating a Node with value [%d]\n", x);
 	stack_t *newNode;
+
 	newNode = (stack_t *)malloc(sizeof(stack_t));
+	printf("Creating a Node with value [%d]\n", x);
+	if (newNode == NULL)
+	{
+		printf("Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
 	newNode->prev = NULL;
 	newNode->next = NULL;
 	newNode->n = x;
 	return (newNode);
 }
+
 /**
-* push - Function to push a node 
-* @value: The value to be pushed
-* @stack: A pointer to a Pointer of a stack 
+* push - Function to push a node
+* @stack: A pointer to a Pointer of a stack
 * @line_number: Line number
 */
 
 void push(stack_t **stack, unsigned int line_number)
 {
+	stack_t *head = NULL, *top = NULL;
 	char *rcvd_value = strtok(NULL, " ");
-	printf("received value: [%s]\n", rcvd_value);
 	int converted_value = strtol(rcvd_value, NULL, 10);
-	stack_t *newNode = createNode(converted_value);
+	stack_t *newNode = (stack_t *)createNode(converted_value);
+
+	printf("received value: [%s]\n", rcvd_value);
 	if (head == NULL)
 		head = top = newNode;
 	else
@@ -50,14 +60,15 @@ void push(stack_t **stack, unsigned int line_number)
 
 /**
  * pop - Function to pop from stack;
- * @stack: A pointer to a Pointer of a stack 
+ * @stack: A pointer to a Pointer of a stack
  * @line_number: Line number
  * return: nothing
 */
 void pop(stack_t **stack, unsigned int line_number)
 {
 	/* Popping Function */
-	stack_t *temp;
+	stack_t *temp, *head = NULL, *top = NULL;
+
 	if (head == NULL || top == NULL)
 		printf("Empty Stack\n");
 	else
@@ -75,22 +86,47 @@ void pop(stack_t **stack, unsigned int line_number)
 
 /**
  * pall - Function to print the values
- * @stack: A pointer to a Pointer of a stack 
+ * @stack: A pointer to a Pointer of a stack
  * @line_number: Line number
 */
 void pall(stack_t **stack, unsigned int line_number)
 {
 	printf("\nDisplaying ===== \n");
-	stack_t *temp;
+	stack_t *temp, *head = NULL, *top = NULL;
+
 	temp = head;
 	if (head == NULL)
 		printf("Empty Stack. Nothing to display\n");
 	while (temp != NULL)
 	{
 		printf("Value: %d \n", temp->n);
-		temp = temp -> next;
+		temp = temp->next;
 	}
-	free (temp);
+	free(temp);
+}
+
+/**
+ * errorCheck - Check for an invalid instruction
+ * @line_number: Line number where the invlaid instruciton appears
+ * @instruction: Instruction to be checked
+*/
+void errorCheck(int line_number, char *instruction)
+{
+	int i, is_valid = 0, INSTRUCTION_COUNT = 3;
+
+	for (i = 0; i < INSTRUCTION_COUNT; i++)
+	{
+		if (strcmp(instruction, VALID_INSTRUCTIONS[i]) == 0)
+		{
+			is_valid = 1;
+			break;
+		}
+	}
+	if (!is_valid)
+	{
+		printf("L%d: Unknown Instruction '%s'\n", line_number, instruction);
+		exit(EXIT_FAILURE);
+	}
 }
 
 #endif /* OP_FUNCS */
