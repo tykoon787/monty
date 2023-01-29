0e253e5bd4648fccf39a1cdf0ba91ebc7dc82319
tykoon787 <tykoon787@protonmail.com>
2023-01-29 16:02:59 +0300

    Complete Monty Header File

--- tests/monty.h
+++ tests/monty.h
@@ -12,9 +12,9 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+		int n;
+		struct stack_s *prev;
+		struct stack_s *next;
 } stack_t;
 
 /**
@@ -27,8 +27,8 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+		char *opcode;
+		void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
 /* ==== FUNCTIONS =====*/
@@ -37,6 +37,10 @@ void *createNode(int x);
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
+void errorCheck(int line_number, char *instruction);
+void pushCall(int line_number);
+void popCall(int line_number);
+void pallCall(int line_number);
 
 
 #endif /* MONTY_H */
