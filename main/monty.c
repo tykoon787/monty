0da929c25ea714f262fe458ed9c572f2fc4a0085
tykoon787 <tykoon787@protonmail.com>
2023-01-30 16:00:17 +0300

    Todo: Pass stack as argument to main

--- main/monty.c
+++ main/monty.c
@@ -22,6 +22,7 @@ int main(int argc, char *argv[])
 	if (argc < 2)
 		printf("Usage Msg\n");
 	else
+	/* To do: Pass stack as an argument to the interpreter*/
 		interpreter(argv[1]);
 
 	return (0);
