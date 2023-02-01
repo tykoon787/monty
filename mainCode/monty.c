0ff6a6785e1ba12bc7e9667da60d309d2121880c
tykoon787 <tykoon787@protonmail.com>
2023-02-01 15:02:21 +0300

    Added Main Code to new folder

---
+++ mainCode/monty.c
@@ -0,0 +1,32 @@
+#include "monty_interpreter.h"
+#include "op_funcs.h"
+#include "monty.h"
+#include "callFuncs.h"
+
+/**
+ * main - Entry Point
+ * @argc: Argument Count
+ * @argv: Arguments
+ * Return: (0) on Success
+*/
+int main(int argc, char *argv[])
+{
+	stack_t **stack;
+
+	stack = (stack_t **)malloc(sizeof(stack_t));
+	if (!stack)
+	{
+		fprintf(stderr, "Malloc Failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if (argc < 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	else
+		interpreter(stack, argv[1]);
+
+	return (0);
+}
