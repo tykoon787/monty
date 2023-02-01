8c6e38b694b0a6ab2756316fa191b2adaccbd6d3
tykoon787 <tykoon787@protonmail.com>
2023-02-01 13:13:25 +0300

    Moved pushError() to op_funcs2.h

--- op_funcs.h
+++ op_funcs.h
@@ -4,6 +4,7 @@
 #include "monty.h"
 #include "monty_interpreter.h"
 #include "callFuncs.h"
+#include "op_funcs.h2"
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -130,29 +131,9 @@ void errorCheck(int line_number, char *instruction)
 	}
 	if (!is_valid)
 	{
-		fprintf(stderr, "L%d: Usage: push integer\n", line_number);
+		fprintf(stderr, "L%d: Uknown Instruction\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-}
-
-/**
- * pushError - Checks for a push error
- * @rcvd_value: Value will be checked if its an int
- * @line_number: Line number where error occurs
- * Return: (converted value) on Success or (0) on error
-*/
-int pushError(char *rcvd_value, int line_number)
-{
-	int converted_value;
-	char *endptr;
-	errno = 0;
-	converted_value = strtol(rcvd_value, &endptr, 10);
-	if (errno == ERANGE || *endptr != '\0')
-	{
-		fprintf(stderr, "L%d: Usage: push integer\n", line_number);
-		return (0);
-	}
-	return (converted_value);
 }
 
 #endif /* OP_FUNCS */
---
+++ op_funcs2.h
@@ -0,0 +1,21 @@
+#include "op_funcs.h"
+
+/**
+ * pushError - Checks for a push error
+ * @rcvd_value: Value will be checked if its an int
+ * @line_number: Line number where error occurs
+ * Return: (converted value) on Success or (0) on error
+*/
+int pushError(char *rcvd_value, int line_number)
+{
+	int converted_value;
+	char *endptr;
+	errno = 0;
+	converted_value = strtol(rcvd_value, &endptr, 10);
+	if (errno == ERANGE || *endptr != '\0')
+	{
+		fprintf(stderr, "L%d: Usage: push integer\n", line_number);
+		return (0);
+	}
+	return (converted_value);
+}
