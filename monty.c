a07aed1ef11ff61766750051f57c6784bb9b8b82
tykoon787 <tykoon787@protonmail.com>
2023-02-01 12:04:12 +0300

    Added Error Msg

--- monty.c
+++ monty.c
@@ -21,9 +21,11 @@ int main(int argc, char *argv[])
 	}
 
 	if (argc < 2)
-		printf("Usage Msg\n");
+	{
+		printf("USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
 	else
-	/* To do: Pass stack as an argument to the interpreter */
 		interpreter(stack, argv[1]);
 
 	return (0);
