35bfc02af31de84ee793809adf165d79da0cd43e
tykoon787 <tykoon787@protonmail.com>
2023-02-01 12:35:17 +0300

    Updated error msgs

--- monty_interpreter.h
+++ monty_interpreter.h
@@ -36,7 +36,7 @@ void interpreter(stack_t **stack, char *filename)
 	fp = fopen(filename, "r");
 
 	if (fp == NULL)
-		fprintf("Error: Can't open file %s", filename);
+		fprintf(stderr, "Error: Can't open file %s", filename);
 	else
 	{
 		while ((bytes_read = getline(&line_buffer, &len, fp)) > 0)
